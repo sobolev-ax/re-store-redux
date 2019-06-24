e3f32e3a9c9ba38386e32648cc114b7c2d51960b
Alexander Sobolev <sobolev.ax@gmail.com>
2019-06-24 10:46:44 +0300

    Added Middlewares

--- src/store.js
+++ src/store.js
@@ -1,38 +1,59 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const stringEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
+const stringMiddleware = (store) => (next) => (action) => {
+    console.log(store.getState());
 
-  const originalDispatch = store.dispatch;
-
-  store.dispatch = (action) => {
-    if (typeof action === "string") {
-      return originalDispatch({
-        type: action,
-      })
-    }
-
-    return originalDispatch(action)
-  }
-  return store;
+  if (typeof action === "string") {
+    return next({
+      type: action,
+  })}
+  return next(action)
 }
 
-const logEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-
-  const originalDispatch = store.dispatch;
-
-  store.dispatch = (action) => {
-    console.log(action.type);
-
-    return originalDispatch(action)
-  }
-  return store;
+const logMiddleware = () => (dispatch) => (action) => {
+  console.log(action.type);
+  return dispatch(action);
 }
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
-
+const store = createStore(reducer, applyMiddleware(
+  stringMiddleware,
+  logMiddleware,
+));
 store.dispatch("HELLO");
+
+///// ENHANCERS
+
+// const stringEnhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+
+//   const originalDispatch = store.dispatch;
+
+//   store.dispatch = (action) => {
+//     if (typeof action === "string") {
+//       return originalDispatch({
+//         type: action,
+//       })
+//     }
+
+//     return originalDispatch(action)
+//   }
+//   return store;
+// }
+
+// const logEnhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+
+//   const originalDispatch = store.dispatch;
+
+//   store.dispatch = (action) => {
+//     console.log(action.type);
+
+//     return originalDispatch(action)
+//   }
+//   return store;
+// }
+
+// const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
 
 export default store;
